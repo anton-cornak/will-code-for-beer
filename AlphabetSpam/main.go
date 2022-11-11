8b75cc1cb0ec79a74aa27e1a3bed0cd34f857d7b
Anton Cornak <79978722+anton-cornak@users.noreply.github.com>
2022-11-11 14:46:17 +0100

    finished alphabed spam

--- AlphabetSpam/main.go
+++ AlphabetSpam/main.go
@@ -1,5 +1,30 @@
 package alphabetspam
 
-func alphabetSpam(in string) []float64 {
-	return []float64{}
+func alphabetSpam(text string) (res []float64) {
+	var whitespaces_count, lowercase_count, uppercase_count, symbol_count int
+
+	for _, c := range text {
+		if c >= 97 && c <= 122 {
+			lowercase_count++
+			continue
+		}
+		if c >= 65 && c <= 90 {
+			uppercase_count++
+			continue
+		}
+		if c == 95 {
+			whitespaces_count++
+			continue
+		}
+		symbol_count++
+	}
+
+	var length = float64(len(text))
+
+	res = append(res, float64(whitespaces_count)/length)
+	res = append(res, float64(lowercase_count)/length)
+	res = append(res, float64(uppercase_count)/length)
+	res = append(res, float64(symbol_count)/length)
+
+	return res
 }
